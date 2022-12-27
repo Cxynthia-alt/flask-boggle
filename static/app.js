642b0d8c227ac66463e10dd500f999285e8d2902
fran <jane08150020@163.com>
2022-12-27 13:49:02 -0800

    3rd commit

--- static/app.js
+++ static/app.js
@@ -1,12 +1,26 @@
+let resultSection = document.querySelector('#result')
 async function getData(word) {
   const res = await axios.get('/validate-guess', { params: { guess: word } });
-  return res
+  return res.data
 }
 
 const form = document.querySelector('#boggle_form');
 form.addEventListener('submit', async function (e) {
   e.preventDefault();
+
   let userInput = document.querySelector('#user_guess_word').value;
-  const result = await getData(userInput)
+  const resultJSON = await getData(userInput)
+  let result = resultJSON[userInput]
+  let msg
+  if (result == 'ok') { msg = "Dingdingding!" }
+  else if (result == 'not-on-board') { msg = 'The word is not on board' }
+  else msg = 'It\'s not a word'
+
+  // front-end
+  let resultDiv = document.createElement('div')
+  resultDiv.textContent = msg
+  resultDiv.setAttribute('class', 'resultDiv')
+  resultSection.appendChild(resultDiv)
+  form.reset();
 
 })
