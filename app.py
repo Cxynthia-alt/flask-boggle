6a56041660d9d75dfccb944052d6497a3940fc5a
fran <jane08150020@163.com>
2023-01-04 15:07:47 -0800

    test if constructor class works

--- app.py
+++ app.py
@@ -12,19 +12,19 @@ debug = DebugToolbarExtension(app)
 boggle_game = Boggle()
 
 
-
 @app.route('/')
 def set_board():
     board = boggle_game.make_board()
     session['board'] = board
-    submit_count=session.get('submit_count',0)
+    submit_count = session.get('submit_count', 0)
     highscore = session.get('highest_score', 0)
-    return render_template('index.html', board=board, highscore=highscore,submit_count=submit_count)
+    return render_template('index.html', board=board, highscore=highscore, submit_count=submit_count)
 
 
 @app.route('/validate-guess')
 def validate_guess():
     guess = request.args["guess"]
+    board = session['board']
     validate_result = boggle_game.check_valid_word(board, guess)
     return jsonify({guess: validate_result})
 
@@ -34,19 +34,19 @@ def score_and_submission():
     # increment the session['submit_count']
     # return session['submit_count']
     # send the highest score back to the front-end
-    
-    #access the current score from front-end
+
+    # access the current score from front-end
     score = request.json["score"]
-    
-    #access the current submit_count again and update/increment it in session
-    submit_count=session.get('submit_count',0)
+
+    # access the current submit_count again and update/increment it in session
+    submit_count = session.get('submit_count', 0)
     session['submit_count'] = submit_count + 1
-    
-    #access the highest score from sessionm, compare with current score, and store the latest highest score in session
+
+    # access the highest score from sessionm, compare with current score, and store the latest highest score in session
     highscore = session.get('highest_score', 0)
     session['highest_score'] = max(score, highscore)
-    
+
     # import pdb
     # pdb.set_trace()
-    #use brokeRecord to represent the True/False in order to access it in the front-end
-    return jsonify(brokeRecord=score>highscore)
+    # use brokeRecord to represent the True/False in order to access it in the front-end
+    return jsonify(brokeRecord=score > highscore)
--- static/app.js
+++ static/app.js
@@ -1,6 +1,5 @@
 // set up variables
 let resultSection = document.querySelector('#result')
-let form = document.querySelector('#boggle_form');
 let resultMsg = document.createElement('div')
 let showScore = document.createElement('div')
 let resultWordList = document.createElement('ul')
@@ -19,65 +18,67 @@ class BoogleGame {
     this.score = 0;
     this.countTime = 0;
     this.timer = setInterval(tick, 1000)
+    this.form = document.querySelector('#boggle_form');
+    this.form.addEventListener('click', this.eventHanderler.bind(this))
   }
 
-//GET reuquest to check the word
-async function getData(word) {
-  const res = await axios.get('/validate-guess', { params: { guess: word } });
-  return res.data
-}
-
-// set a timer
-
-async function tick() {
-  this.countTime++
-  if (this.countTime == 60) {
-    clearInterval(this.timer)
-    await addData()
-    this.countTime = 0
+  //GET reuquest to check the word
+  async getData(word) {
+    const res = await axios.get('/validate-guess', { params: { guess: word } });
+    return res.data
   }
 
-}
+  // set a timer
 
+  async tick() {
+    this.countTime++
+    if (this.countTime == 60) {
+      clearInterval(this.timer)
+      await addData()
+      this.countTime = 0
+    }
 
-// form submission
-
-form.addEventListener('submit', async function (e) {
-  e.preventDefault();
-
-
-  // get the result after checking the word in back-end
-  let userInput = document.querySelector('#user_guess_word').value;
-  let resultJSON = await getData(userInput)
-  let result = resultJSON[userInput]
-  let msg
-  if (result == 'ok') {
-    msg = "Dingdingding!"
-    this.score += userInput.length
-    resultWord.innerText = userInput
-  } else if (result == 'not-on-board') { msg = 'The word is not on board' }
-  else msg = 'It\'s not a word'
-
-  //get the highest score
-
-  // front-end
-  resultMsg.innerText = msg
-
-  resultWordList.append(resultWord)
-  resultSection.append(resultMsg)
-
-  form.reset();
-})
-
-//POST request to update score/count_submit
-async function addData() {
-  const res = await axios.post('/score-and-submission-times', { score: this.score })
-  if (res.data.brokeRecord) {
-    showScore.innerText = 'New record: ' + this.score
-  } else {
-    showScore.innerText = 'Final record: ' + this.score
   }
-  resultSection.append(showScore)
-}
+
+
+  // form submission
+
+  async eventHanderler(e) {
+    e.preventDefault();
+
+
+    // get the result after checking the word in back-end
+    let userInput = document.querySelector('#user_guess_word').value;
+    let resultJSON = await getData(userInput)
+    let result = resultJSON[userInput]
+    let msg
+    if (result == 'ok') {
+      msg = "Dingdingding!"
+      this.score += userInput.length
+      resultWord.innerText = userInput
+    } else if (result == 'not-on-board') { msg = 'The word is not on board' }
+    else msg = 'It\'s not a word'
+
+    //get the highest score
+
+    // front-end
+    resultMsg.innerText = msg
+
+    resultWordList.append(resultWord)
+    resultSection.append(resultMsg)
+
+    form.reset();
+  }
+
+  //POST request to update score/count_submit
+  async addData() {
+    const res = await axios.post('/score-and-submission-times', { score: this.score })
+    if (res.data.brokeRecord) {
+      showScore.innerText = 'New record: ' + this.score
+    } else {
+      showScore.innerText = 'Final record: ' + this.score
+    }
+    resultSection.append(showScore)
+  }
 
 }
